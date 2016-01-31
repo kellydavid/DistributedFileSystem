3e52a122d65540441d29c077f38386877f38c4ac
kellydavid <kellydavid178@gmail.com>
2016-01-31 19:57:35 +0000

    Added initial client and locking service modules

---
+++ dfs-locking-service/src/main/java/com/kellydavid/dfs/ls/App.java
@@ -0,0 +1,13 @@
+package com.kellydavid.dfs.ls;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
